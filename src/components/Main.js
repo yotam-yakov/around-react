f2e59ef3c680a600cefb6c690533c295376d2722
Yotam7298 <yoyo729800@gmail.com>
2022-07-14 08:54:02 +0300

    Ported api and updated user info

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,23 @@
+import React from "react";
+import api from "../utils/api";
+
 export default function Main(props) {
+  const [userName, setUserName] = React.useState("");
+  const [userAbout, setUserAbout] = React.useState("");
+  const [userAvatar, setUserAvatar] = React.useState("");
+
+  api.getUserInfo().then((data) => {
+    setUserName(data.name);
+    setUserAbout(data.about);
+    setUserAvatar(data.avatar);
+  });
+
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__avatar-cover">
           <img
-            src="#"
+            src={userAvatar}
             alt="profile avatar of the current user"
             className="profile__avatar"
             id="profile-avatar"
@@ -17,14 +30,18 @@ export default function Main(props) {
         </div>
         <div className="profile__info">
           <h1 className="profile__name">
-            <span id="profile-name" className="profile__name-text"></span>
+            <span id="profile-name" className="profile__name-text">
+              {userName}
+            </span>
             <button
               type="button"
               className="profile__edit-button"
               onClick={props.onEditProfileClick}
             ></button>
           </h1>
-          <p id="profile-about" className="profile__about"></p>
+          <p id="profile-about" className="profile__about">
+            {userAbout}
+          </p>
         </div>
         <button
           type="button"
---
+++ src/utils/api.js
@@ -0,0 +1,132 @@
+class Api {
+  constructor(options) {
+    this._options = options;
+  }
+
+  _verifyResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    let err;
+    return Promise.reject(err);
+  }
+
+  reportError(err) {
+    console.log(`Something went wrong, Error: ${err.status}`);
+  }
+
+  _logInfo(res) {
+    console.log("Request was successful:");
+    console.log(res);
+  }
+
+  getAllInfo() {
+    return Promise.all([this.getUserInfo(), this.loadCards()]).then(
+      (values) => {
+        return values;
+      }
+    );
+  }
+
+  getUserInfo() {
+    return fetch(`${this._options.baseUrl}/users/me`, {
+      headers: this._options.headers,
+    })
+      .then(this._verifyResponse)
+      .then((res) => {
+        this._logInfo(res);
+        return res;
+      });
+  }
+
+  loadCards() {
+    return fetch(`${this._options.baseUrl}/cards`, {
+      headers: this._options.headers,
+    })
+      .then(this._verifyResponse)
+      .then((res) => {
+        this._logInfo(res);
+        return res;
+      });
+  }
+
+  editProfileInfo(userNewInfo) {
+    return fetch(`${this._options.baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._options.headers,
+      body: JSON.stringify({
+        name: userNewInfo.name,
+        about: userNewInfo.about,
+      }),
+    })
+      .then(this._verifyResponse)
+      .then((res) => {
+        this._logInfo(res);
+        return res;
+      });
+  }
+
+  editProfilePicture(avatarUrl) {
+    return fetch(`${this._options.baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._options.headers,
+      body: JSON.stringify({
+        avatar: avatarUrl.avatar,
+      }),
+    })
+      .then(this._verifyResponse)
+      .then((res) => {
+        this._logInfo(res);
+        return res;
+      });
+  }
+
+  addNewCard(cardNewInfo) {
+    return fetch(`${this._options.baseUrl}/cards`, {
+      method: "POST",
+      headers: this._options.headers,
+      body: JSON.stringify({
+        name: cardNewInfo.title,
+        link: cardNewInfo.link,
+      }),
+    })
+      .then(this._verifyResponse)
+      .then((res) => {
+        this._logInfo(res);
+        return res;
+      });
+  }
+
+  deleteCard(cardId) {
+    return fetch(`${this._options.baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._options.headers,
+    })
+      .then(this._verifyResponse)
+      .then((res) => {
+        this._logInfo(res);
+      });
+  }
+
+  changeCardLike(cardId, method) {
+    return fetch(`${this._options.baseUrl}/cards/likes/${cardId}`, {
+      method: method,
+      headers: this._options.headers,
+    })
+      .then(this._verifyResponse)
+      .then((res) => {
+        this._logInfo(res);
+        return res;
+      });
+  }
+}
+
+const api = new Api({
+  baseUrl: "https://around.nomoreparties.co/v1/cohort-3-en",
+  headers: {
+    authorization: "f800af66-4bca-42fd-8139-117d10b5a510",
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
